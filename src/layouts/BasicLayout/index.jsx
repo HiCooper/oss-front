b4297d1f41db2efa9e78eb85e037a240a3acc6cc
HiCooper <294237781@qq.com>
2019-05-26 17:11:43 +0800

    basic layout

--- src/layouts/BasicLayout/index.jsx
+++ src/layouts/BasicLayout/index.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import { Layout, Menu, Icon } from 'antd';
 import MainRouter from './MainRouter';
+import { headMenuConfig, sideMenuConfig } from '../../menuConfig';
+import './index.scss';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 export default class BasicLayout extends Component {
     static displayName = 'BasicLayout';
@@ -11,9 +17,66 @@ export default class BasicLayout extends Component {
 
     render() {
       return (
-        <div>
-          <MainRouter />
-        </div>
+        <Layout className="basic-layout">
+          <Header className="header">
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['1']}
+              style={{ lineHeight: '64px' }}
+            >
+              {
+                headMenuConfig.map((item, index) => {
+                  return (
+                    <Menu.Item key={index + 1}>{item.name}</Menu.Item>
+                  );
+                })
+              }
+            </Menu>
+          </Header>
+          <Layout>
+            <Sider width={200} className="side">
+              <Menu
+                mode="inline"
+                defaultOpenKeys={['1']}
+                defaultSelectedKeys={['1']}
+                style={{ height: '100%', borderRight: 0 }}
+              >
+                {
+                  sideMenuConfig.map((item, index) => {
+                    return (
+                      <SubMenu
+                        key={index + 1}
+                        title={(
+                          <span>
+                            <Icon type={item.icon} />
+                            {item.name}
+                          </span>
+                        )}
+                      >
+                        {
+                          item.children && item.children.length > 0 ? (
+                            item.children.map((i) => {
+                              return (
+                                <Menu.Item key={i.name}>{i.name}</Menu.Item>
+                              );
+                            })
+                          ) : null
+                        }
+                      </SubMenu>
+                    );
+                  })
+                }
+              </Menu>
+            </Sider>
+            <Layout className="main-content">
+              <Content className="content">
+                <MainRouter />
+              </Content>
+            </Layout>
+          </Layout>
+        </Layout>
       );
     }
 }
---
+++ src/menuConfig.js
@@ -0,0 +1,57 @@
+const headMenuConfig = [
+  {
+    name: 'nav1',
+    path: '',
+  }, {
+    name: 'nav2',
+    path: '',
+  }, {
+    name: 'nav3',
+    path: '',
+  },
+];
+
+const sideMenuConfig = [
+  {
+    name: 'subnav1',
+    path: '',
+    icon: 'user',
+    children: [
+      {
+        name: 'option1',
+        path: '',
+      }, {
+        name: 'option2',
+        path: '',
+      }, {
+        name: 'option3',
+        path: '',
+      }, {
+        name: 'option4',
+        path: '',
+      },
+    ],
+  },
+  {
+    name: 'subnav2',
+    path: '',
+    icon: 'notification',
+    children: [
+      {
+        name: 'option5',
+        path: '',
+      }, {
+        name: 'option6',
+        path: '',
+      }, {
+        name: 'option7',
+        path: '',
+      }, {
+        name: 'option8',
+        path: '',
+      },
+    ],
+  },
+];
+
+export { headMenuConfig, sideMenuConfig };
