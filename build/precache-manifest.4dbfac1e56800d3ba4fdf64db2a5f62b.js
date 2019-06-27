088f4ea0ec609acde9e580bc73397196940e66f1
berry_cooper <berry_cooepr@163.com>
2019-06-27 09:13:32 +0800

    'x'

---
+++ build/precache-manifest.4dbfac1e56800d3ba4fdf64db2a5f62b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fa83cd7e62f3eab1701f06e91cb2fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c4d5ec55ff0d294b0fa",
+    "url": "/static/css/2.b87527a6.chunk.css"
+  },
+  {
+    "revision": "e6b1cae7abd80157b919",
+    "url": "/static/css/main.2b35659e.chunk.css"
+  },
+  {
+    "revision": "1c4d5ec55ff0d294b0fa",
+    "url": "/static/js/2.f61fea80.chunk.js"
+  },
+  {
+    "revision": "e6b1cae7abd80157b919",
+    "url": "/static/js/main.83991630.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c382430f673ad2237bbf19e5c8a4b00",
+    "url": "/static/media/NotFound-260-260.8c382430.png"
+  },
+  {
+    "revision": "832a4d3ff41654d5d835bad73c2784d2",
+    "url": "/static/media/bg.832a4d3f.jpg"
+  },
+  {
+    "revision": "f429eca790b54d4327bb44a8db001df2",
+    "url": "/static/media/main_bg.f429eca7.png"
+  },
+  {
+    "revision": "a11091bf38151170cd4f9aab66ed003f",
+    "url": "/static/media/main_bg_night.a11091bf.png"
+  }
+]);
