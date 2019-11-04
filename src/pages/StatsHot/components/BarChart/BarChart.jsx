da1378348a48d8d7c4547c06f8df87e65a93e94f
berry_cooper <berry_cooepr@163.com>
2019-11-04 13:01:15 +0800

    update chart

--- src/pages/StatsHot/components/BarChart/BarChart.jsx
+++ src/pages/StatsHot/components/BarChart/BarChart.jsx
@@ -22,29 +22,16 @@ export default class BarChart extends Component {
       }).sort((a, b) => a.value - b.value);
     }
 
-    const label = {
-      offset: 12, // 数值，设置坐标轴文本 label 距离坐标轴线的距离
-      // 设置文本的显示样式，还可以是个回调函数，回调函数的参数为该坐标轴对应字段的数值
-      rotate: -30, // 文本旋转角度
-      textStyle: {
-        textAlign: 'center', // 文本对齐方向，可取值为： start center end
-        fill: '#404040', // 文本的颜色
-        fontSize: '12', // 文本大小
-        fontWeight: 'bold', // 文本粗细
-        textBaseline: 'top', // 文本基准线，可取 top middle bottom，默认为middle
-      },
-    };
-
     return (
       <div>
-        <Chart height={400} data={temp} forceFit>
+        <Chart height={window.innerHeight - 283} data={temp} forceFit>
           <Coord transpose />
           <Axis
             name="name"
-            rotate={30}
-            label={label}
+            label={{
+              offset: 12,
+            }}
           />
-          <Axis name="value" />
           <Tooltip />
           <Geom type="interval" position="name*value" />
         </Chart>
